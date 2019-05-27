985473860f5cbabba94868ce9dabaa0cbe3bcda7
hbs0802 <1052879618@qq.com>
2019-05-27 23:46:17 +0800

    feat【上传-裁剪】: 新增图片裁剪功能

--- src/components/upload.vue
+++ src/components/upload.vue
@@ -13,14 +13,51 @@
     </el-upload>
     <p>图片的裁剪</p>
     <p>vue-cropper地址: <a href="https://github.com/Agontuk/vue-cropperjs" target="_blank">vue-cropper</a></p>
-    
+    <!-- <vue-cropper
+      ref="cropper"
+      :src="imgSrc"
+      alt="Source Image"
+      :cropmove="cropImage">
+    </vue-cropper> -->
+    <div class="crop-img">
+      <img :src="cropImg" alt="" class="pre-img">
+      <div class="btn">
+        选择图片
+        <input type="file" name="image" accept="image/*" @change="setImage" class="crop-input"/>
+      </div>
+    </div>
+    <el-dialog title="裁剪图片"
+      :visible.sync="dialogVisible"
+      width="30%">
+    <vue-cropper
+      ref="cropper"
+      :src="imgSrc"
+      alt="Source Image"
+      style="width: 100%; height: 300px;"
+      :ready="cropImage"
+      :zoom="cropImage"
+      :cropmove="cropImage">
+    </vue-cropper>
+    <span slot="footer">
+      <el-button @click="cancelCrop">取消</el-button>
+      <el-button type="primary" @click="dialogVisible = false">确定</el-button>
+    </span>
+    </el-dialog>
   </div>
 </template>
 <script>
+// Local
+import VueCropper from 'vue-cropperjs';
+import 'cropperjs/dist/cropper.css';
+
 export default {
   data() {
     return {
-      imageUrl: ''
+      defaultUrl: require('../assets/logo.png'),
+      imageUrl: '',
+      dialogVisible: false,
+      cropImg: '',
+      imgSrc: '',
     };
   },
   methods: {
@@ -38,8 +75,36 @@ export default {
         this.$message.error('上传头像图片大小不能超过 2MB!');
       }
       return isJPG && isLt2M;
-    }
-  }
+    }, 
+    setImage(e) {
+        const file = e.target.files[0];
+        if (!file.type.includes('image/')) {
+          alert('error');
+          return;
+        }
+        const reader = new FileReader();
+        reader.onload = (event) => {
+          this.dialogVisible = true;
+          this.imgSrc = event.target.result;
+          this.$refs.cropper && this.$refs.cropper.replace(event.target.result);
+        }
+        reader.readAsDataURL(file);
+    },
+    cropImage() {
+      this.cropImg = this.$refs.cropper.getCroppedCanvas().toDataURL();
+    },
+    cancelCrop() {
+      this.dialogVisible = false;
+      this.cropImg = this.defaultUrl;
+    },
+
+  },
+  components: {
+    VueCropper
+  },
+  created() {
+    this.cropImg = this.defaultUrl;
+  },
 }
 </script>
 <style>
@@ -65,5 +130,38 @@ export default {
     width: 178px;
     height: 178px;
     display: block;
+  }
+  .crop-img {
+    display: flex;
+    align-items: flex-end;
+  }
+  .pre-img {
+    width: 100px;
+    height: 100px;
+    background: #f8f8f8;
+    border: 1px solid #eee;
+    border-radius: 5px;
+  }
+  .crop-img .btn {
+    position: relative;
+    width: 100px;
+    height: 40px;
+    line-height: 40px;
+    padding: 0 20px;
+    margin-left: 30px;
+    background-color: #409eff;
+    color: #fff;
+    font-size: 14px;
+    border-radius: 4px;
+    box-sizing: border-box;
+  }
+  .crop-input {
+    position: absolute;
+    width: 100px;
+    height: 40px;
+    left: 0;
+    top: 0;
+    opacity: 0;
+    cursor: pointer;
   }
 </style>
